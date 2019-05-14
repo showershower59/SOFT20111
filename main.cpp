b4410cca96a98b0c4aa73553557345263fd9a330
Ong Jian Wei <32693777+showershower59@users.noreply.github.com>
2019-05-14 18:14:18 +0800

    This Commit

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
 <<<<<<< HEAD
 	cout << "hello world2222"<< endl;
 =======
-	cout << "Every programmer first step"<< endl;
+	cout << "Every programmer No step"<< endl;
 	cout << "Every programmer second step"<< endl;
 >>>>>>> main
 	system("pause");
